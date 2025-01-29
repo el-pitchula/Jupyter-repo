0ae62b565f5b277bcacb736ce20a21c5faa27db0
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2025-01-29 16:44:47 -0300

    main

--- pendulum.py
+++ pendulum.py
@@ -1,18 +1,20 @@
 import random
 import gym
+import numpy as np
 
 env = gym.make("CartPole-v1", render_mode="human")
 
-episodes = 10
+episodes = 1000
 for episode in range(1, episodes+1):
-    state = env.reset()
+    state, _ = env.reset()  # Agora env.reset() retorna dois valores: state e info
     done = False
     score = 0
     
     while not done:
         action = random.choice([0, 1])
-        _, reward, done, -__= env.step(action)
+        observation, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated  # O episódio pode terminar por terminated ou truncated
         score += reward
         env.render()
     print(f"Episode: {episode}, Score: {score}")
-env.close()    
+env.close()
